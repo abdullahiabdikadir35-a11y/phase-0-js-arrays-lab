68ee0b094379536d6cfc79f647c74e044eaf520b
abdullahiabdikadir35-a11y <abdullahiabdikadir35@gmail.com>
2025-11-23 15:46:54 +0300

    Completed product inventory lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,15 +1,3 @@
-// Write your code here
-
-
-
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  
-
 let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
 function logFirstProduct() {
@@ -20,15 +8,22 @@ function addProduct(productName) {
   products.push(productName);
 }
 
-
 function updateProductName(index, newName) {
   products[index] = newName;
 }
 
-
 function removeLastProduct() {
   products.pop();
 }
+
+module.exports = {
+  products,
+  logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct
+};
+
 
 
 
