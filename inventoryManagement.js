6687ba1bfcc3c9135398fde4bf3fd45a3d153599
abdullahiabdikadir35-a11y <abdullahiabdikadir35@gmail.com>
2025-11-23 14:52:49 +0300

    completed product inventory lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -8,5 +8,28 @@ module.exports = {
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+  
+
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+
+function removeLastProduct() {
+  products.pop();
+}
+
+
+
+
